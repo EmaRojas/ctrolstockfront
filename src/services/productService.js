ff86b02efb8b3625de1adc77c3383e5ba5ab3817
Ema Rojas <erojas@sancorseguros.com>
2025-09-27 20:40:52 -0300

    cambios en product

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API_URL = "https://ctrlstockbackend-git-main-emarojas-projects.vercel.app/products";
 export const getProducts = async () => {
-    const res = await axios.get(API_URL);
+    const res = await axios.get(API_URL, { withCredentials: true });
     return res.data;
 };
 export const getProduct = async (id) => {
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "https://ctrlstockbackend-git-main-emarojas-projects.vercel.app/products";
 
 export const getProducts = async () => {
-  const res = await axios.get(API_URL);
+  const res = await axios.get(API_URL,{withCredentials: true});
   return res.data;
 };
 
