71f094b48a111f805a25df05504b35b827263802
Ema Rojas <erojas@sancorseguros.com>
2025-09-27 18:21:24 -0300

    cambios url

--- src/services/cashRegisterService.ts
+++ src/services/cashRegisterService.ts
@@ -1,7 +1,7 @@
 // services/cashRegisterService.ts
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/cashRegister"; // 🔁 ajustá el puerto si es distinto
+const API_URL = "https://ctrlstockbackend-git-main-emarojas-projects.vercel.app/cashRegister"; // 🔁 ajustá el puerto si es distinto
 
 // 🟢 Abrir caja
 export const openCashRegister = async (initialAmount: number, openedBy: string) => {
